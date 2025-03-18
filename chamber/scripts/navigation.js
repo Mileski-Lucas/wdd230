b1a867646f1d022b1be4f2a830d9d52049195ad1
Mileski-Lucas <lucas.m_47@hotmail.com>
2025-03-18 00:27:24 -0300

    chamber milestone

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,5 +1,14 @@
-// Hamburguer Menu
 document.getElementById('menu-button').addEventListener('click', function() {
     const navMenu = document.getElementById('nav-menu');
+    const menuButton = this;
+
     navMenu.classList.toggle('active');
+    menuButton.classList.toggle('open');
+
+    if (menuButton.classList.contains('open')) {
+        menuButton.textContent = '✕'; // Close icon
+    } else {
+        menuButton.textContent = '☰'; // Hamburger icon
+    }
+
 });
